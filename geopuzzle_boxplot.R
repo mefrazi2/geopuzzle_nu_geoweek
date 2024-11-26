aa4692d648fe59ebecedba4ce2cc1e26d7f3f693
mech frazier <41482472+mefrazi2@users.noreply.github.com>
2024-11-26 11:52:22 -0600

    Add files via upload

--- geopuzzle_boxplot.R
+++ geopuzzle_boxplot.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(RColorBrewer)
 library(tidyverse)
 
-time_data <- read.csv("times22.csv")
+time_data <- read.csv("times24.csv")
 
 theme_set(theme_bw(base_size = 16))
 
@@ -11,9 +11,9 @@ times <- time_data %>%
   geom_boxplot()+
   geom_jitter(width=0.1, alpha=0.5)+
   scale_y_log10()+
-  theme(plot.title = element_text (hjust = 0.5)) +
-  labs(title = "Distribution of Times for Geopuzzle Challenge '22",
-       x="Puzzle Type", 
+  theme(plot.title = element_text (hjust = 0.0)) +
+  labs(title = "Distribution of Times for Geopuzzle Challenge '24",
+       x="", 
        y = "Time (minutes)") +
   scale_x_discrete(labels = function(x) str_wrap(x, width = 15)) + # wrapping x-axis labels
   #scale_x_discrete(guide = guide_axis(n.dodge=2))+
